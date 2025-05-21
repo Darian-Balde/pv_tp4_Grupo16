de950b6c2f9c00ac3d456d14a50f495053245a9f
Ignacio Jes <ignaciojes@gmail.com>
2025-05-20 23:09:44 -0300

    feat: Agregado componente SearchBar en RamaIgnacio

---
+++ ListaProdu/src/components/SearchBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './SearchBar.css'
+
+const SearchBar = ({ searchType, setSearchType, searchQuery, setSearchQuery }) => {
+  return (
+    <div className="search-bar">
+      <label>
+        <input
+          type="radio"
+          value="descripcion"
+          checked={searchType === 'descripcion'}
+          onChange={(e) => setSearchType(e.target.value)}
+        />
+        Descripción
+      </label>
+      <label>
+        <input
+          type="radio"
+          value="id"
+          checked={searchType === 'id'}
+          onChange={(e) => setSearchType(e.target.value)}
+        />
+        ID
+      </label>
+      <input
+        type="text"
+        className="search-input"
+        placeholder="Buscar producto..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default SearchBar
